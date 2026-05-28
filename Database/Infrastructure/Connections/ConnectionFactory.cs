585bf488ccd2afdd62fbda1865850f0326c98b43
maxfabiano <64711484+maxfabiano@users.noreply.github.com>
2026-05-28 20:31:02 -0300

    ajuste

---
+++ Database/Infrastructure/Connections/ConnectionFactory.cs
@@ -0,0 +1,26 @@
+﻿using Database.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Infrastructure.Connections
+{
+    public class ConnectionFactory : IDbConnectionFactory
+    {
+        private readonly string _connectionString;
+
+        public ConnectionFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        /*
+        public IDbConnection CreateConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+        */
+    }
+}
